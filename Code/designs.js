7bbf1ec1252587ceb4f0b82f8c9f8ff03d594799
lk-v <lkv.ukr@gmail.com>
2018-01-13 22:28:19 +0100

    Added: colour cells when click, colour cells when mouse drag, event listener to Colour Picker. Default colour for colour picker is changed, default values of Height and Width of grid are predefined. Added max and min values to the grid.

--- Code/designs.js
+++ Code/designs.js
@@ -2,6 +2,14 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+var isPointerDown = false;
+$("#pixelCanvas").mousedown(function(ev) {
+  isPointerDown = true;
+});
+
+$("#pixelCanvas").mouseup(function(ev) {
+  isPointerDown = false;
+});
 
 function makeGrid(rows, columns) {
   for (var i = 0; i < rows; i++) {
@@ -10,6 +18,12 @@ function makeGrid(rows, columns) {
     for (var j = 0; j < columns; j++) {
       var td = document.createElement('td');
       tr.append(td);
+      td.addEventListener("pointerenter", function(ev) {
+        if (isPointerDown === true) {
+          colourCell($(this));
+        }
+
+      });
     }
   }
 }
@@ -25,5 +39,22 @@ $("#sizePicker").submit(function(event) {
   var width = $("#input_width").val();
   makeGrid(height, width);
   event.preventDefault();
+
+});
+
+function colourCell(cell) {
+  cell.css("background", selectedColour);
+}
+
+$("#pixelCanvas").on("click", "td", function(event) {
+  event.preventDefault();
+  colourCell($(this));
+});
+
+var defaultColour = "green";
+var selectedColour = $("#colorPicker").val();
+
+$("#colorPicker").on("change", function(event) {
+  selectedColour = $("#colorPicker").val();
 
 });
