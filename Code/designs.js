9293ccac5887ab7bc8c09cf1647e667654d82320
lk-v <lkv.ukr@gmail.com>
2018-01-13 01:35:05 +0100

    Added dynamic grid creation and clear grid function.

--- Code/designs.js
+++ Code/designs.js
@@ -3,16 +3,27 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
+function makeGrid(rows, columns) {
+  for (var i = 0; i < rows; i++) {
+    var tr = document.createElement('tr');
+    $("#pixelCanvas").append(tr);
+    for (var j = 0; j < columns; j++) {
+      var td = document.createElement('td');
+      tr.append(td);
+    }
+  }
 }
 
-function test() {
-  $("#pixelCanva").append("Hello my project");
+function clearGrid() {
+  $("#pixelCanvas").empty();
 }
 
-$( "#sizePicker" ).submit(function( event ) {
-  alert( "Handler for .submit() called." );
+$("#sizePicker").submit(function(event) {
+  console.log("Size picker called");
+  clearGrid();
+  var height = $("#input_height").val();
+  var width = $("#input_width").val();
+  makeGrid(height, width);
   event.preventDefault();
-  console.log("Hello");
+
 });
